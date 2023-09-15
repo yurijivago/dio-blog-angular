826d570ce2ee9b4bf5eaf6024aea223a0a598466
Yuri Jivago <yurijivagoads@gmail.com>
2023-09-15 13:04:43 -0300

    Desacoplando os componentes; Criando outras páginas

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string ="";
+
+  @Input()
+  cardTitle:string ="";
+
+  @Input()
+  cardDescription:string ="";
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "aaa",
+        "title": "saiu novo filme",
+        "description": "jlaksjçlfdkja çskljçlkjçalkj klfjaksçdl fjaç~sdj fçlkadsf"
+    },
+    {
+        "id": 1,
+        "photo": "aaa",
+        "title": "saiu novo filme",
+        "description": "jlaksjçlfdkja çskljçlkjçalkj klfjaksçdl fjaç~sdj fçlkadsf"
+    }
+]
